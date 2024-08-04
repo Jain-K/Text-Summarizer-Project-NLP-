fac89d8b274d5a403af58f5b8a0727b7d54f2898
Jain-K <kartikj21@iitk.ac.in>
2024-08-04 12:28:59 +0530

    Requirements and Installation Done Again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
+REPO_NAME = "Text-Summarizer-Project-NLP-"
 AUTHOR_USER_NAME = "Jain-K"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "kartikj21@iitk.ac.in"
